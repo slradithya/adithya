d1459c4b525729b35d643d127ceb6903e1fbab5e
r.santhosh <r.santhosh@LAPTOP-VKQFDPF1>
2022-01-27 20:43:48 +0530

    trying to committ

---
+++ adithya/src/adithya/addition.java
@@ -0,0 +1,12 @@
+package adithya;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c =10+34;
+		System.out.print(c);
+
+	}
+
+}
---
+++ adithya/src/adithya/stu1.java
@@ -0,0 +1,11 @@
+package adithya;
+
+public class stu1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello adithya");
+
+	}
+
+}
